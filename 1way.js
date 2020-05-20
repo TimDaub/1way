b687694fec47308e8ceea88d53b2977dfd373e26
Tim Daubenschütz <tim@daubenschuetz.de>
2020-05-20 21:35:22 +0200

    Initial commit

---
+++ 1way.js
@@ -0,0 +1,36 @@
+// @format
+
+const selector = {
+  css: "bind",
+  plain: "bind"
+};
+class Store extends WeakMap {
+  set(...args) {
+    const [elem, v] = args;
+    if (elem instanceof HTMLInputElement) {
+      elem.value = v;
+    } else {
+      elem.innerHTML = v;
+    }
+    return super.set(...args);
+  }
+}
+const store = new Store();
+
+export const useState = (init, name) => {
+  const fns = Array.from(document.querySelectorAll(`*[bind='{${name}}']`)).map(
+    funFactory(init)
+  );
+  console.log(fns);
+  const setter = newValue => fns.forEach(fn => fn(newValue));
+  return [init, setter];
+};
+
+const funFactory = init => {
+  return elem => {
+    if (!store.get(elem)) {
+      store.set(elem, init);
+    }
+    return v => store.set(elem, v);
+  };
+};
