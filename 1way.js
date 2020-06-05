67088e494e06e59ec7fd04443eb058f529e9fa8b
Tim Daubenschütz <tim@daubenschuetz.de>
2020-06-05 14:55:20 +0200

    Implement "integrate" functionality

--- 1way.js
+++ 1way.js
@@ -1,37 +1,94 @@
 // @format
 
-const selector = {
-  css: "bind",
-  plain: "bind",
-  regexp: new RegExp("(.*)\{([^\)]+)\}(.*)")
-};
+class Selector {
+  /*
+    `selectors` interface
+  
+      - query: the xpath-style string to find a specific DOM node.
+      - plain: the name of the attribute in plain text.
+      - regexp: A regular expression to match and validate the value of the
+          attribute.
+      - exclusive: A boolean expressing the exlusitivity of a tag in relation to
+        any other tag.
+      - exec: A function that transforms the matched element in-place.
+  */
+  constructor(query, plain, regexp, exclusive, fun) {
+    this.query = query;
+    this.plain = plain;
+    this.regexp = regexp;
+    this.exclusive = exclusive;
+    this._fun = fun;
+    this.storage = {};
+  }
+
+  exec(elem, value) {
+    this._fun(this, elem, value);
+  }
+}
+
+const selectors = [
+  new Selector(
+    name => `*[bind*='{${name}}']`,
+    "bind",
+    new RegExp("(.*){([^)]+)}(.*)"),
+    true,
+    (context, elem, value) => {
+      // TODO: Find out if we can change `elem` in-place
+      const [_, pre, match, suf] = elem
+        .getAttribute(context.plain)
+        .match(context.regexp);
+      if (elem instanceof HTMLInputElement) {
+        elem.value = `${pre}${value}${suf}`;
+      } else {
+        elem.innerHTML = `${pre}${value}${suf}`;
+      }
+    }
+  ),
+  new Selector(
+    name => `*[integrate*='{${name}}']`,
+    "integrate",
+    // NOTE: We're required to use a dynamic regexp here.
+    new RegExp(),
+    true,
+    (context, elem, value) => {
+      if (!context.storage.originalText) {
+        context.storage.originalText = elem.textContent;
+      }
+      const name = elem.getAttribute("integrate");
+      elem.textContent = context.storage.originalText.replaceAll(name, value);
+    }
+  )
+];
 
 class Store extends WeakMap {
   set(...args) {
-    const [elem, v] = args;
-    const [_, pre, match, suf] = elem.getAttribute("bind").match(selector.regexp);
-    if (elem instanceof HTMLInputElement) {
-      elem.value = `${pre}${v}${suf}`;
-    } else {
-      elem.innerHTML= `${pre}${v}${suf}`;
-    }
+    const [elem, value] = args;
+    const selector = selectors.find(s => elem.hasAttribute(s.plain));
+    selector.exec(elem, value);
+
     return super.set(...args);
   }
 }
 const store = new Store();
 
-export const useState = (init, name) => {
-  const fns = Array.from(document.querySelectorAll(`*[bind*='{${name}}']`)).map(
-    funFactory(init)
-  );
+export const useState = (initialValue, name) => {
+  const fns = selectors
+    .map(({ query }) => {
+      // NOTE: We needed to make `query` lazily evaluatable. Hence we've wrapped
+      // it inside a function.
+      return Array.from(document.querySelectorAll(query(name))).map(
+        funFactory(initialValue)
+      );
+    })
+    .flat();
   const setter = newValue => fns.forEach(fn => fn(newValue));
-  return [init, setter];
+  return [initialValue, setter];
 };
 
-const funFactory = init => {
+const funFactory = initialValue => {
   return elem => {
     if (!store.get(elem)) {
-      store.set(elem, init);
+      store.set(elem, initialValue);
     }
     return v => store.set(elem, v);
   };
